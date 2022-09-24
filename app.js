2a778283db4a1b8bcd3c6445d560dd78310e69a8
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-24 18:40:22 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var express=require('express')
+
+app=express();
+
+//post method
+app.post("/sample",function (req,res){
+    res.send("This is simple post request");
+})
+
+
+//post in header method
+
+app.post("/nine",function (req,res){
+   let userName= req.header("userName");
+   let password= req.header("password");
+   res.send("User name:"+userName,+ "Password"+password);
+   res.status(201,).end("Data Send Successful")
+});
+
+
+// url quest in post method
+
+
+app.post('/search',function (req,res){
+    let firstName=req.query.firstName;
+    let lastName=req.query.lastName;
+    res.send(firstName+ " "+lastName);
+
+});
+
+
+//Get Request Header
+
+app.get('/ten',function (req,res){
+    let firstName=req.header('firstName');
+    let lastName=req.header('lastName');
+    res.end(firstName+ " "+ lastName);
+
+})
+
+
+//url query get request
+app.get("/eleven",function (req,res){
+ let firstName=   req.query.firstName;
+ let lastName=   req.query.lastName;
+ res.end(firstName+ " "+ lastName);
+
+})
+
+
+app.listen(8000,function (){
+    console.log("Server Run Successfully!")
+})
